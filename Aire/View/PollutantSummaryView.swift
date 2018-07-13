4f4cc2f6b423d8112f79fae8efe0ba93ebf907b1
Natalia <garcia.doe@gmail.com>
2018-07-13 14:57:28 -0700

    Stopped adding subviews programmatically (for full report alert). Made pollutant summary more customizable

--- Aire/View/PollutantSummaryView.swift
+++ Aire/View/PollutantSummaryView.swift
@@ -15,9 +15,38 @@ class PollutantSummaryView: NibView {
 	@IBOutlet var pollutantIndexLabel: UILabel!
 	@IBOutlet var pollutantLevelView: CircleView!
 	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		self.pollutantIndexLabel.font = self.pollutantIndexLabel.font.withSize(frame.size.width * 22.0/75.0)
+	}
+	
 	func update(pollutant: Pollutant) {
 		pollutantTitleLabel.text = pollutant.title
 		pollutantIndexLabel.text = String(pollutant.aqi)
 		pollutantLevelView.layer.borderColor = AirQualityUtility.scale[pollutant.aqi].color.cgColor
+	}
+	
+	@IBInspectable var heading: Int = 0 {
+		didSet {
+			switch(self.heading) {
+			case 1:
+				self.pollutantTitleLabel.font = self.pollutantIndexLabel.font.withSize(24)
+			case 2:
+				self.pollutantTitleLabel.font = self.pollutantIndexLabel.font.withSize(26)
+			case 3:
+				self.pollutantTitleLabel.font = self.pollutantIndexLabel.font.withSize(28)
+			case 4:
+				self.pollutantTitleLabel.font = self.pollutantIndexLabel.font.withSize(30)
+			case 5:
+				self.pollutantTitleLabel.font = self.pollutantIndexLabel.font.withSize(32)
+			case 6:
+				self.pollutantTitleLabel.font = self.pollutantIndexLabel.font.withSize(34)
+			case 7:
+				self.pollutantTitleLabel.font = self.pollutantIndexLabel.font.withSize(36)
+			default:
+				self.pollutantTitleLabel.font = self.pollutantIndexLabel.font.withSize(22)
+			}
+			setNeedsDisplay()
+		}
 	}
 }
