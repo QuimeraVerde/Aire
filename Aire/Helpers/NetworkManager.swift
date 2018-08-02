ddc41b28d67a4ee14c63ab8baf11a0eceb72a5b4
Natalia <garcia.doe@gmail.com>
2018-08-02 10:15:26 -0700

    Setup NetworkManager and UIViewButton

--- Aire/Helpers/NetworkManager.swift
+++ Aire/Helpers/NetworkManager.swift
@@ -4,6 +4,64 @@
 //
 //  Created by Natalia García on 8/1/18.
 //  Copyright © 2018 QuimeraVerde. All rights reserved.
-//
+// 	https://blog.pusher.com/handling-internet-connection-reachability-swift/
 
 import Foundation
+import Reachability
+
+class NetworkManager: NSObject {
+	
+	var reachability: Reachability!
+	
+	// Create a singleton instance
+	static let shared: NetworkManager = { return NetworkManager() }()
+	
+	override init() {
+		super.init()
+		
+		// Initialise reachability
+		reachability = Reachability()!
+		
+		// Register an observer for the network status
+		NotificationCenter.default.addObserver(
+			self,
+			selector: #selector(networkStatusChanged(_:)),
+			name: .reachabilityChanged,
+			object: reachability
+		)
+		
+		do {
+			// Start the network status notifier
+			try reachability.startNotifier()
+		} catch {
+			print("Unable to start notifier")
+		}
+	}
+	
+	@objc func networkStatusChanged(_ notification: Notification) {
+		// Do something globally here!
+	}
+	
+	static func stopNotifier() -> Void {
+		do {
+			// Stop the network status notifier
+			try (NetworkManager.shared.reachability).startNotifier()
+		} catch {
+			print("Error stopping notifier")
+		}
+	}
+	
+	// Network is reachable
+	static func isReachable(completed: @escaping (NetworkManager) -> Void) {
+		if (NetworkManager.shared.reachability).connection != .none {
+			completed(NetworkManager.shared)
+		}
+	}
+	
+	// Network is unreachable
+	static func isUnreachable(completed: @escaping (NetworkManager) -> Void) {
+		if (NetworkManager.shared.reachability).connection == .none {
+			completed(NetworkManager.shared)
+		}
+	}
+}
--- Aire/View/NibClass/UIViewButton.swift
+++ Aire/View/NibClass/UIViewButton.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+import UIKit
+
+class UIViewButton: UIView {
+	
+	private let disposeBag = DisposeBag()
+	private var tapRecognizer: UITapGestureRecognizer?
+	
+	var onTap: (UIGestureRecognizer) -> Void {
+		get {
+			return self.onTap
+		}
+		set {
+			self.setTapGesture(newValue)
+			self.enabled = true
+		}
+	}
+	
+	var enabled: Bool {
+		get {
+			return self.enabled
+		}
+		set {
+			if newValue {
+				self.addTapGesture()
+				self.alpha = 1.0
+			}
+			else {
+				self.removeTapGesture()
+				self.alpha = 0.5
+			}
+		}
+	}
+	
+	private func addTapGesture() {
+		if let tapRecognizer = self.tapRecognizer {
+			self.addGestureRecognizer(tapRecognizer)
+		}
+	}
+	
+	private func removeTapGesture() {
+		if let tapRecognizer = self.tapRecognizer {
+			self.removeGestureRecognizer(tapRecognizer)
+		}
+	}
+	
+	private func setTapGesture(_ onTap: @escaping (UIGestureRecognizer) -> Void) {
+		let tapRecognizer = UITapGestureRecognizer()
+		tapRecognizer.rx.event
+			.bind(onNext: { recognizer in
+				onTap(recognizer)
+			}).disposed(by: disposeBag)
+		
+		self.tapRecognizer = tapRecognizer
+	}
+}
