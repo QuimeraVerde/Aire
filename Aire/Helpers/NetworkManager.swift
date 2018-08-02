8e966fdde46bc00326851a20181d553545096824
Natalia <garcia.doe@gmail.com>
2018-08-02 10:12:52 -0700

    Added NetworkManager and UIViewButton for buttons with content inside

---
+++ Aire/Helpers/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Aire
+//
+//  Created by Natalia García on 8/1/18.
+//  Copyright © 2018 QuimeraVerde. All rights reserved.
+//
+
+import Foundation
---
+++ Aire/View/NibClass/UIViewButton.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewButton.swift
+//  Aire
+//
+//  Created by Natalia García on 8/2/18.
+//  Copyright © 2018 QuimeraVerde. All rights reserved.
+//
+
+import Foundation
